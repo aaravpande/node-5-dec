9bb0fddb75fe9758d91b9490036ecb4b83475eab
Aarav pandey <aaravpandey056@gmail.com>
2024-05-11 08:59:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import App from './App'; // Assuming you have an App component in App.js
+
+// Render the App component into the root element of the HTML document
+//ReactDOM.render(<App />, document.getElementById('root'));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>React App</title>
+</head>
+<body>
+  <div id="root"></div>
+  <script src="path/to/your/index.js"></script>
+</body>
+</html>
