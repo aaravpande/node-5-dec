288af31e73fa7b86d7c2f060ed7e8c38a240e6c2
Aarav pandey <aaravpandey056@gmail.com>
2024-05-11 08:36:47 +0530

    Add files via upload

---
+++ counterSlice.js
@@ -0,0 +1,13 @@
+//import { configureStore } from '@reduxjstoolkit';
+//import counterReducer from './counterSlice'; // Assuming you have a counter slice
+
+// Create the Redux store
+//const store = configureStore({
+  reducer: {
+   // counter: counterReducer,
+    // Add other reducers here if you have more slices
+  }
+  // Optionally configure middleware, dev tools, etc.
+;
+
+//export default store;
