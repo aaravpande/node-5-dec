298721d2200f466191806aa06bf17a65b0a8a423
Aarav pandey <aaravpandey056@gmail.com>
2024-05-11 12:55:20 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My React App</title>
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>
