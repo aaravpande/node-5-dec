739d669fde906e39a9efd43ed95a8755b6a54c54
Aarav pandey <aaravpandey056@gmail.com>
2024-05-13 17:45:20 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit, nisi nec tincidunt venenatis, magna leo tempor orci, ut vestibulum magna magna eu odio. Donec tempus risus sit amet purus tincidunt, eu pretium risus suscipit. Vestibulum sagittis dui in suscipit fermentum. Integer non justo vitae sapien cursus fermentum. Suspendisse potenti.</p>
+      <p>Phasellus scelerisque dui eget nunc viverra, nec varius leo condimentum. Nulla facilisi. Sed vehicula orci nec risus commodo scelerisque. Vivamus euismod, nisl sed viverra fringilla, turpis massa posuere purus, id dictum nulla quam eu dolor.</p>
+    </div>
+  );
+};
+
+export default About;
